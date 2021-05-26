d2e565a35a64dbf1589bf69a033dde000d6e0651
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2021-05-26 20:47:58 -0300

    Commit

--- contador/src/components/CorpoTela/CorpoTela.jsx
+++ contador/src/components/CorpoTela/CorpoTela.jsx
@@ -1,33 +1,46 @@
 import React, { Component } from "react";
 import "./style.css";
 
-let numero = 0;
-const atualizaNumero = () => {
-  document.getElementById("numero").textContent = ++numero;
-};
-
-const resetaNumero = () => {
-  let resetados = document.getElementById("resetados");
-  if(!resetados.textContent){
-    resetados.textContent = numero;
-  } else {
-    resetados.textContent += ", " + numero
-  }
-  
-  numero = 0;
-  document.getElementById("numero").textContent = numero;
-};
-
 class CorpoTela extends Component {
+  constructor() {
+    super();
+    this.state = {
+      numero: 0,
+      resetados: "",
+    };
+  }
+  _atualizaNumero() {
+    console.log(this.state)
+    let novoNumero = this.state.numero;
+    const novoState = {
+      numero: ++novoNumero,
+    };
+    this.setState(novoState);
+  }
+  _resetaNumero() {
+    let numeroResetado;
+    if (this.state.resetados === "") {
+      numeroResetado = this.state.numero;
+    } else {
+      numeroResetado = this.state.resetados + ", " + this.state.numero;
+    }
+    const novoState = {
+      numero: 0,
+      resetados: numeroResetado,
+    };
+    this.setState(novoState);
+  }
   render() {
     return (
       <section className="corpoPagina">
         <article>Clique no botão para incrementar um numero!</article>
-        <button onClick={atualizaNumero}>Botão</button>
-        <p id="numero" onClick={resetaNumero}>0</p>
+        <button onClick={this._atualizaNumero.bind(this)}>Botão</button>
+        <p id="numero" onClick={this._resetaNumero.bind(this)}>
+          {this.state.numero}
+        </p>
         <article>Clique sobre o numero para reseta-lo!</article>
         <article>O numero foi resetado em:</article>
-        <article id="resetados"></article>
+        <article>{this.state.resetados}</article>
       </section>
     );
   }

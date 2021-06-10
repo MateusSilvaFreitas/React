da77c534d4a338df22535233bd7d5624e5e4f049
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2021-06-09 23:04:37 -0300

    Commit

--- senai/src/routes.js
+++ senai/src/routes.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import {Browser, BrowserRouter, Route, Switch} from 'react-router-dom';
-import Teste from './paginas/teste/index';
-import HelloMessage from './components/HelloMessage';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Teste from "./paginas/teste/index";
+import HelloMessage from "./components/HelloMessage";
+import Livros from "./paginas/Livro/index";
+import CadastroLivro from "./components/FormularioCadastroLivro";
+import CadastroUsuario from "./components/FormularioCadastroUsuario";
+import MenuBar from "./components/MenuBar";
 
-function Routes(){
-    return(
+function Routes() {
+  return (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" exact={true} component={HelloMessage}/>
-            <Route path="/teste" component={Teste}/>
-        </Switch>
+    <MenuBar/>
+      <Switch>
+        <Route path="/" exact={true} component={HelloMessage} />
+        <Route path="/teste" component={Teste} />
+        <Route path="/livro" component={Livros} />
+        <Route path="/cadastroLivro" component={CadastroLivro} />
+        <Route path="/cadastroUsuario" component={CadastroUsuario} />        
+      </Switch>
     </BrowserRouter>
-    );
+  );
 }
 
-export default Routes; 
+export default Routes;
